59aa3baea18b3de86b0e082addcc6d769bbd2620
Ivan Stevkovski <ivan@qbit.solutions>
2020-05-02 04:19:40 +0200

    Updates

--- precache-manifest.71bd0591ea75f0961157aa4236ff71a4.js
+++ precache-manifest.71bd0591ea75f0961157aa4236ff71a4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aa96bce54001fedbc367587ce840d741",
+    "revision": "51c77b51fd70455a3d7efbbb839219a3",
     "url": "/react-json-parser/index.html"
   },
   {
-    "revision": "a9a4934b88d89ed2bc63",
+    "revision": "480f034f75aa4c6026d6",
     "url": "/react-json-parser/static/css/main.cf25fa89.chunk.css"
   },
   {
-    "revision": "a82f3c7026ec97d33657",
-    "url": "/react-json-parser/static/js/2.3132aa04.chunk.js"
+    "revision": "6a98792645715d2db66a",
+    "url": "/react-json-parser/static/js/2.68b93a66.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/react-json-parser/static/js/2.3132aa04.chunk.js.LICENSE.txt"
+    "url": "/react-json-parser/static/js/2.68b93a66.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a9a4934b88d89ed2bc63",
-    "url": "/react-json-parser/static/js/main.4383aac9.chunk.js"
+    "revision": "480f034f75aa4c6026d6",
+    "url": "/react-json-parser/static/js/main.d68941ca.chunk.js"
   },
   {
     "revision": "2b77e3a40d83bc454c48",
